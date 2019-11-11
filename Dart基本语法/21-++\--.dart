a82b588874ae998ff56c788ff6481932c3335d07
SLCoderZeng <359297567@qq.com>
2019-11-11 15:39:11 +0800

    add 23-while\do...while

--- Dart基本语法/21-++\--.dart
+++ Dart基本语法/21-++\--.dart
@@ -7,8 +7,8 @@ void main(){
     在赋值运算里面 如果++ -- 写在前面 这时候先运算 再赋值，如果++ --写在后面 先赋值后运行运算
 
 
-    var a=10;
-    var b=a--;
+    var a = 10;
+    var b = a--;
 
     print(a);  //9
     print(b);  //10
@@ -16,31 +16,28 @@ void main(){
   */
 
 
-    // var a=10;
+    // var a = 10;
 
-    // a++;   //a=a+1;
+    // a++;   //a = a+1;
 
     // print(a);
 
 
 
-    // var a=10;
-    // a--;    //a=a-1;
+    // var a = 10;
+    // a--;    //a = a-1;
     // print(a);
 
-
-
-
-    // var a=10;
-    // var b=a++;
+    // var a = 10;
+    // var b = a++;
 
     // print(a);  //11
     // print(b);  //10
 
 
 
-    // var a=10;
-    // var b=++a;
+    // var a = 10;
+    // var b = ++a;
 
     // print(a);  //11
     // print(b);  //11
@@ -48,23 +45,23 @@ void main(){
 
 
     
-    // var a=10;
-    // var b=--a;
+    // var a = 10;
+    // var b = --a;
 
     // print(a);  //9
     // print(b);  //9
 
 
 
-    // var a=10;
-    // var b=a--;
+    // var a = 10;
+    // var b = a--;
 
     // print(a);  //9
     // print(b);  //10
 
 
 
-    var a=10;
+    var a  =  10;
 
     ++a;
 
