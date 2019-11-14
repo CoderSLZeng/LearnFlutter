0da6d5c2d51a8e26cd0ab99ca6eeb7c943aa8656
SLCoderZeng <359297567@qq.com>
2019-11-14 14:37:25 +0800

    给 Text 组件增加一些装饰

--- Flutter入门实战/demo/lib/main.dart
+++ Flutter入门实战/demo/lib/main.dart
@@ -21,7 +21,13 @@ class MyApp extends StatelessWidget {
     return Center(
       child: Text( 
         "我是 Dart 一个文本内容",
-        textDirection: TextDirection.ltr
+        textDirection: TextDirection.ltr, // ltr = left to right 文本从左往右对齐
+        style: TextStyle( // 给 Text 组件增加一些装饰
+          fontSize: 30.0, // Dart 数字都是 Number 类型，必须要小数点
+          fontWeight: FontWeight.bold,
+          // color: Colors.red
+          color: Color.fromRGBO(0, 255, 0, 0.5)
+        )
     )
     );
   }
