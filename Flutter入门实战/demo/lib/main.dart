afd0dd77876fad28f3c502d9fca53748bc9dfe0e
SLCoderZeng <359297567@qq.com>
2019-11-18 10:43:31 +0800

    add ListView组件的使用’

--- Flutter入门实战/demo/lib/main.dart
+++ Flutter入门实战/demo/lib/main.dart
@@ -35,11 +35,116 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return CustomCenter();
-    // return CustomContainer();
+    // returnCustomListView CustomContainer();
     // return AssetImage();
     // return networkImage();   
     // return BorderRadiusImage(); 
-    return ClipOvalImage();
+    // return ClipOvalImage();
+    // return BaseListView();
+    // return HorizontalListView();
+    return DynamicListView();
+  }
+}
+
+// 动态列表 组件
+class DynamicListView extends StatelessWidget {
+
+  List list = new List();
+  DynamicListView({Key key}): super(key: key) {
+    for (var i = 0; i < 20; i++) {
+      list.add("这是第${i}条数据");
+    }
+    print(list);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: this.list.length,
+      itemBuilder: (context, index) {
+        print(context);
+        return ListTile(
+          leading: Icon(Icons.phone),
+          title: Text("${list[index]}")
+        );
+      }
+    );
+  }
+}
+
+// 水平列表 组件
+class HorizontalListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200.0,
+      margin: EdgeInsets.all(5),
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: <Widget>[
+          Container(
+            width: 180.0,
+            color: Colors.lightBlue
+          ),
+          Container(
+            width: 180.0,
+            color: Colors.amber,
+            child: ListView(
+              children: <Widget>[
+                Image.asset("imgs/02.jpeg"),
+                Text('这是一个文本信息',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 16.0)
+                )
+              ]
+            )
+          ),
+          Container(
+            width: 180.0,
+            color: Colors.deepOrange
+          ),
+          Container(
+            width: 180.0,
+            color: Colors.deepPurpleAccent
+          )
+        ]
+      )
+    );
+  }
+}
+
+// 基础列表 组件
+class BaseListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.phone), // Icon 图标组件
+            title: Text("this is title0", style: TextStyle(fontSize: 28.0)),
+            subtitle: Text("this is lsubtitle0")
+          ),
+          ListTile(
+            title: Text("this is title1"),
+            subtitle: Text("this is subtitle1"),
+            trailing: Icon(Icons.phone)
+          ),
+          ListTile(
+            title: Text("this is title2"),
+            subtitle: Text("this is subtitle2")            
+          ),
+          ListTile(
+            title: Text("this is title3"),
+            subtitle: Text("this is subtitle3")
+          ),
+          ListTile(
+            title: Text("this is title4"),
+            subtitle: Text("this is subtitle4")
+          )
+        ]
+      )
+    );
   }
 }
 
